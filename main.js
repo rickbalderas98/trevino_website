f5a740a8388edb8bf6a93750626a6c8b16fdb6b2
rickb <rick.balderas98@gmail.com>
2026-01-17 15:31:35 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
